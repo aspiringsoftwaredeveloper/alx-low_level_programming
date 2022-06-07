0227c46d5c33dd5de36db534137f290107a84006
Dzunani <dzunaniaa6@gmail.com>
2022-06-07 05:43:22 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Prints _putchar followed by new line 
  *
  * Return: Always 0
  */
@@ -19,4 +19,3 @@ int main(void)
 
 	return (0);
 }
-
