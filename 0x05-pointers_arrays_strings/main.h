0114455bfb7ef3e071ea6a1e025c43f4c572962e
Dzunani <dzunaniaa6@gmail.com>
2022-06-13 07:47:54 -0700

    puts

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
-_putchar.c
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-
+void print_rev(char *s);
