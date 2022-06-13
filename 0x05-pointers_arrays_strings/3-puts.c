b947ecca8d5ffb7f8a3df628ef6375d2552e82c6
Dzunani <dzunaniaa6@gmail.com>
2022-06-13 05:56:50 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string 
+ * @str: string input
+ * Return: string
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[1] != '\0'; i++)
+	{
+		_putchar(str[1]);
+	}
+	_putchar('\n');
+}
