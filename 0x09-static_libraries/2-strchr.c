f2625a843d7a3677bedc1d5d84ded96ae4de1bb8
Dzunani <dzunaniaa6@gmail.com>
2022-06-28 02:02:35 -0700

    create static library

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - function that locates string
+ * @c: character
+ * @s: string
+ *
+ * Return: a pointer to the first occurence of the character
+ */
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	while (1)
+	{
+		a = *s++;
+		if (a == c)
+		{
+
+			return (s - 1);
+		}
+		if (a == 0)
+		{
+			return (NULL);
+		}
+	}
+}
