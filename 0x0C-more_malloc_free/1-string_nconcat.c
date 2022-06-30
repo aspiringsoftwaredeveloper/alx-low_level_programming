ce8c778f5e9372aabddccb98ef221ec298812422
Dzunani <dzunaniaa6@gmail.com>
2022-06-30 11:16:51 -0700

    string nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -38,7 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < l1; i++)
 		*(s + i) = *(s1 + i);
 
-	for (i = 0, i = l1; i < l2' i++)
+	for (i = 0, i = l1; i < l2; i++)
 		*(s + j) = *(s2 + i);
 
 	*(s + j) = '\0';
