9b046b870bcafc8d0cde7095f0fda9707d6b2b26
Dzunani <dzunaniaa6@gmail.com>
2022-07-04 08:06:26 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,6 +19,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 char *dup_strint(char *s);
-void free_dog(dot_t *d);
+void free_dog(dog_t *d);
 
 #endif
