cedd4222c131d9257c7e25b16c3ed87d8acd2c84
Dzunani <dzunaniaa6@gmail.com>
2022-07-04 15:31:06 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,50 +1,89 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
- * new_dog - creates a new dog
- * @name: dog's name
- * @age: dog's ages
- * @owner: dog's owner
+ *  du_string - Duplicates a string passed into a newly allocated space in
+ *  memory
+ *  @s: Pointer to the first character in the source string 
  *
- * Return: a pointer to the structure
+ *  Return: Pointer to the first character in the newly allocated and copied
+ *  string
  */
+char *dup_string(char *s)
+{
+	int len, i;
+	char *ret;
 
+	for (len = 0; s[len]; len++)
+		;
+	ret = malloc(sizeof(char) * (len + 1));
+	if (ret == NULL)
+		return (NULL);
+
+	for (i = 0; s[i]; i++)
+		ret[i] = s[i];
+	ret[i] = '\0';
+
+	return (ret);
+}
+
+/**
+ * new_dog - Makes a new dogwith the given parameters
+ * @name: Pointer to the first character of the string of the name of the dog
+ * to be created
+ * @age: Age of the dog to be created as a float
+ * @owner: Pointer to the first character of the string of the owner of the dog
+ * to be created
+ *
+ * Return: Pointer to the new dog `dog_t` with all of the given attributes
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i, j, k;
-	dog_t *p;
+	char *tname, *towner;
+	dog_t *ret;
 
-	p = malloc(sizeof(dog_t));
+	if (name == NULL || owner == NULL)
+		return (NULL);
 
-	if (p == NULL)
-	{
-		free(p);
+	ret = malloc(sizeof(dog_t));
+	if (ret == NULL)
+		return (NULL);
+	ret->age = age;
+
+	tname = dup_string(name);
+	if (tname == NULL)
+        {
+		free_dog(ret);
 		return (NULL);
 	}
-	for (i = 0; name[i]; i++)
-		;
-	for (j = 0; owner[j]; j++)
-		;
-	p->name = malloc(i + 1);
-	p->owner = malloc(j + 1);
+	ret->name = tname;
 
-	if (p->name == NULL || p->owner == NULL)
+	towner = dup_string(owner);
+	if (towner == NULL)
 	{
-		free(p->name), free(p->owner), free(p);
+		free_dog(ret);
 		return (NULL);
 	}
-	for (k = 0; k > i; k++)
-	{
-		p->name[k] = name[k];
-	}
-	p->name[k] = '\0';
-	for (k = 0; k < j; k++)
-	{
-		p->owner[k] = owner[k];
-	}
-	p->owner[k] = '\0';
-	p->age = age;
-	return (p);
+
+	ret->owner = towner;
+
+	return (ret);
+}
+
+/**
+ * free_dog - Frees all memory allocated by a dog_t struct
+ * @d: Pointer to the struct to be freed
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name != NULL)
+		free(d->name);
+
+	if (d->owner != NULL)
+		free(d->owner);
+
+	free(d);
 }
