dd38bb3d1ead5c9f1276a1a9556f53d565ef3b7d
Dzunani <dzunaniaa6@gmail.com>
2022-07-04 13:57:46 -0700

    free dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dog.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: dog's name
+ * @age: dog's ages
+ * #owner: dog's owner
+ *
+ * Return: a pointer to the structure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	
