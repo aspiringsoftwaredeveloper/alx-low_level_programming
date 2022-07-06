cf46a2cbd2fc41a70ae5eb7e4ade2ffeb1495ddf
Dzunani <dzunaniaa6@gmail.com>
2022-07-06 11:00:35 -0700

    Task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,12 +20,17 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	if ((argv[2][1] != 0) || ((argv[2][0] != '+') && (argv[2][0] != '-')
-	        && (argv[2][0] != '*') && (argv[2][0] != '/') && (argv[2][0] != '%' )))
+	        && (argv[2][0] != '*') && (argv[2][0] != '/') && (argv[2][0] != '%')))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '/' || (arggv[2][0] == '%')) && atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	v = get_op_func(argv[2]) (atoi(argv[1])),
-	                printf("%\n", v);
+	v = get_op_func(argv[2]) (atoi(argv[1]), atoi(argv[3]));
+	                printf("%d\n", v);
 	return (0);
 }
