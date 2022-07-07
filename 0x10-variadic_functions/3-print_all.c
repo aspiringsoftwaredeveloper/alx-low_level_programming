7e79a221f6e483943ee4b44cd6be2afcf461dda8
Dzunani <dzunaniaa6@gmail.com>
2022-07-07 12:36:23 -0700

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -29,7 +29,7 @@ void print_all(const char * const format, ...)
 		switch (format[counter])
 		{
 	        case 'c':
-			printf("%c", va_arg(myLisy, int)), i = 1;
+			printf("%c", va_arg(myList, int)), i = 1;
 			break;
 		case 'i':
 		        printf("%d", va_arg(myList, int)), i = 1;
